5af449ff136f3b32d2c509dcc5b73a28299ef218
saiful7i <saiful7i3301@gmail.com>
2025-05-13 00:07:05 +0600

    add validtion globaly for post api

--- Program.cs
+++ Program.cs
@@ -10,16 +10,17 @@ builder.Services.AddControllers();
 builder.Services.Configure<ApiBehaviorOptions>(options => {
     options.InvalidModelStateResponseFactory = context => {
                 var errors = context.ModelState
-                .Where(e => e.Value.Errors.Count > 0)
+                .Where(e => e.Value != null && e.Value.Errors.Count > 0)
                 .Select(e => new 
                 {
                     Field = e.Key,
-                    Message = e.Value.Errors.Select(x => x.ErrorMessage).ToArray()
+                    Errors = e.Value != null ? e.Value.Errors.Select(x => x.ErrorMessage).ToArray() : new string[0]
                 }).ToList();
-                // var errorString = string.Join("; ",errors.
-                // Select(e => $"{e.Field} : {string.Join(", ",e.Message)}" ));
+                var errorsString = string.Join("; ",errors.
+                Select(e => $"{e.Field} : {string.Join(", ",e.Errors)}" ));
                 return new BadRequestObjectResult(new {
-
+                    Message = "Validation failed",
+                    Errors = errorsString
                 });
     };
 });
