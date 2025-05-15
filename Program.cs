43c4ee4dce60361cefb49ea48da09756647643e9
saiful7i <saiful7i3301@gmail.com>
2025-05-15 23:41:45 +0600

    create an error response globaly

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using asp_net_ecommerce_web_api.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -7,21 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 //add services to the controller
 builder.Services.AddControllers();
 
-builder.Services.Configure<ApiBehaviorOptions>(options => {
-    options.InvalidModelStateResponseFactory = context => {
-                var errors = context.ModelState
-                .Where(e => e.Value != null && e.Value.Errors.Count > 0)
-                .Select(e => new 
-                {
-                    Field = e.Key,
-                    Errors = e.Value != null ? e.Value.Errors.Select(x => x.ErrorMessage).ToArray() : new string[0]
-                }).ToList();
+builder.Services.Configure<ApiBehaviorOptions>(options => 
+{
+    options.InvalidModelStateResponseFactory = context => 
+    {
+                // var errors = context.ModelState
+                // .Where(e => e.Value != null && e.Value.Errors.Count > 0)
+                // .Select(e => new 
+                // {
+                //     Field = e.Key,
+                //     Errors = e.Value != null ? e.Value.Errors.Select(x => x.ErrorMessage).ToArray() : new string[0]
+                // }).ToList();
                 // var errorsString = string.Join("; ",errors.
                 // Select(e => $"{e.Field} : {string.Join(", ",e.Errors)}" ));
-                return new BadRequestObjectResult(new {
-                    Message = "Validation failed",
-                    Errors = errors 
-                });
+               var errors = context.ModelState
+                .Where(e => e.Value != null && e.Value.Errors.Count > 0)
+                .SelectMany(e => e.Value.Errors.Select(x=>x.ErrorMessage)).ToList();  
+                
+                return new BadRequestObjectResult(ApiResponse.ErrorResponse));
     };
 });
 
